0e25469193c2d2f2a307019920228ee37ff443d8
Alena Bachyla <alena_bachyla@epam.com>
2022-07-09 00:55:15 +0200

    add mocks

--- product-service/handlers/getProductsById.js
+++ product-service/handlers/getProductsById.js
@@ -1,13 +1,40 @@
-export const getProductsById = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Products by ID',
-                input: event,
-            },
-            null,
-            2
-        ),
-    };
+import products from '../mocks/products.json';
+
+const getProduct = (productId) => new Promise((resolve, reject) => {
+    const product = products.find((item) => item.id === productId);
+
+    if (product) {
+        resolve(product);
+    } else {
+        reject({
+            statusCode: 404,
+            message: "Product is not found",
+        });
+    }
+})
+
+export const getProductsById = async (event = {}) => {
+    let response = {};
+
+    try {
+        const id = event.pathParameters && event.pathParameters.productId;
+
+        if (!id) {
+            throw {statusCode: 500, message: 'id is not provided'}
+        }
+
+        const product = await getProduct(id);
+
+        response = {
+            statusCode: 200,
+            body: JSON.stringify(product),
+        }
+    } catch (error) {
+        response = error || {
+            statusCode: 500,
+            message: 'Something went wrong'
+        };
+    }
+
+    return response;
 };
--- product-service/handlers/getProductsList.js
+++ product-service/handlers/getProductsList.js
@@ -1,13 +1,25 @@
-export const getProductsList = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Products list',
-                input: event,
-            },
-            null,
-            2
-        ),
-    };
+import products from '../mocks/products.json';
+
+const getProducts = () => new Promise((resolve) => {
+    resolve(products);
+});
+
+export const getProductsList = async () => {
+    let response = {};
+
+    try {
+        const list = await getProducts();
+
+        response = {
+            statusCode: 200,
+            body: JSON.stringify(list),
+        }
+    } catch {
+        response = {
+            statusCode: 500,
+            message: 'Something went wrong',
+        }
+    }
+
+    return response;
 };
